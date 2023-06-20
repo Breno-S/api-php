15aef4f26ff34efca8d399438b52104ef4630e10
Breno-S <114250412+Breno-S@users.noreply.github.com>
2023-06-20 01:24:58 -0300

    Bootstrap UI
    send help (>_<)

--- api.php
+++ api.php
@@ -33,6 +33,16 @@ $dbname = "teste-api";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Verifique se a conexão foi estabelecida com sucesso
+if ($conn->connect_error) {
+    $response = array(
+        'status' => 'error',
+        'message' => 'Failed to connect to MySQL: ' . $conn->connect_error
+    );
+    echo json_encode($response);
+    exit;
+}
+
 // Verifique o método da requisição
 $method = $_SERVER['REQUEST_METHOD'];
 
@@ -52,98 +62,80 @@ if ($method == 'GET') {
     $params = $_GET;
 
     if ($endpoint == 'users') {
-        // Verifique se a conexão foi estabelecida com sucesso
-        if ($conn->connect_error) {
-            $response = array(
-                'status' => 'error',
-                'message' => 'Failed to connect to MySQL: ' . $conn->connect_error
-            );
-        } else {
-            // Execute a consulta para obter os dados dos usuários
-            $sql = "SELECT * FROM users";
-            $result = $conn->query($sql);
+        // Execute a consulta para obter os dados dos usuários
+        $sql = "SELECT * FROM users";
+        $result = $conn->query($sql);
 
-            // Verifique se a consulta retornou resultados
-            if ($result->num_rows > 0) {
-                $users = array();
+        // Verifique se a consulta retornou resultados
+        if ($result->num_rows > 0) {
+            $users = array();
 
-                // Itere pelos resultados e adicione os usuários ao array
-                while ($row = $result->fetch_assoc()) {
-                    $user = array(
-                        'id' => $row['id'],
-                        'name' => $row['name'],
-                        'email' => $row['email']
-                    );
-
-                    $users[] = $user;
-                }
-
-                $response = array(
-                    'status' => 'success',
-                    'users' => $users
+            // Itere pelos resultados e adicione os usuários ao array
+            while ($row = $result->fetch_assoc()) {
+                $user = array(
+                    'id' => $row['id'],
+                    'name' => $row['name'],
+                    'email' => $row['email']
                 );
-            } else {
-                $response = array(
-                    'status' => 'success',
-                    'users' => []
-                );
+
+                $users[] = $user;
             }
 
-            // Feche a conexão com o banco de dados
-            $conn->close();
+            $response = array(
+                'status' => 'success',
+                'users' => $users
+            );
+        } else {
+            $response = array(
+                'status' => 'success',
+                'users' => []
+            );
         }
+
+        // Feche a conexão com o banco de dados
+        $conn->close();
     }
 
     if ($endpoint == 'products/images') {
+        // Execute a consulta para obter os dados dos usuários
+        $sql = "SELECT * FROM products";
+        $result = $conn->query($sql);
+        
+        // Verifique se a consulta retornou resultados
+        if ($result->num_rows > 0) {
+            $products = array();
 
-        // Verifique se a conexão foi estabelecida com sucesso
-        if ($conn->connect_error) {
-            $response = array(
-                'status' => 'error',
-                'message' => 'Failed to connect to MySQL: ' . $conn->connect_error
-            );
-        } else {
-            // Execute a consulta para obter os dados dos usuários
-            $sql = "SELECT * FROM products";
-            $result = $conn->query($sql);
-            
-            // Verifique se a consulta retornou resultados
-            if ($result->num_rows > 0) {
-                $products = array();
+            // Itere pelos resultados e adicione os usuários ao array
+            while ($row = $result->fetch_assoc()) {
 
-                // Itere pelos resultados e adicione os usuários ao array
-                while ($row = $result->fetch_assoc()) {
-
-                    $product = array(
-                        'id' => $row['id'],
-                        'name' => $row['name'],
-                        'price' => $row['price'],
-                        'image' => $row['image'],
-                        'imageUrl' => $row['image_url'],
-                    );
-
-                    $products[] = $product;
-                }
-
-                $response = array(
-                    'status' => 'success',
-                    'products' => $products
+                $product = array(
+                    'id' => $row['id'],
+                    'name' => $row['name'],
+                    'price' => $row['price'],
+                    'image' => $row['image'],
+                    'imageUrl' => $row['image_url'],
                 );
-            } else {
-                $response = array(
-                    'status' => 'success',
-                    'products' => []
-                );
+
+                $products[] = $product;
             }
 
-            // Feche a conexão com o banco de dados
-            $conn->close();
+            $response = array(
+                'status' => 'success',
+                'products' => $products
+            );
+        } else {
+            $response = array(
+                'status' => 'success',
+                'products' => []
+            );
         }
+
+        // Feche a conexão com o banco de dados
+        $conn->close();
     }
 }
 
 if ($method == 'POST') {
-
     // Verifique o endpoint solicitado
     $endpoint = $_POST['endpoint'];
 
@@ -151,46 +143,40 @@ if ($method == 'POST') {
     $params = $_POST;
 
     if ($endpoint == 'users/profile-image') {
+        // Verifica se o arquivo foi enviado corretamente
+        if (isset($_FILES['imageFile']) && $_FILES['imageFile']['error'] === UPLOAD_ERR_OK) {
+            $targetDir = './uploads';
+            $targetFile = $targetDir . basename($_FILES['imageFile']['name']);
+            $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
 
-        // Verifique se a conexão foi estabelecida com sucesso
-        if ($conn->connect_error) {
-            $response = array(
-                'status' => 'error',
-                'message' => 'Failed to connect to MySQL: ' . $conn->connect_error
-            );
-        } else {
-            // Check if a file was uploaded
-            if (isset($_FILES['imageFile'])) {
-                $file = $_FILES['imageFile'];
+            // Verifica se o arquivo é uma imagem
+            $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+            if (in_array($imageFileType, $allowedExtensions)) {
+            
+                // Move o arquivo para o diretório de armazenamento
+                if (move_uploaded_file($_FILES['imageFile']['tmp_name'], $targetFile)) {
 
-                // Extract the file information
-                $fileTmp = $file['tmp_name'];
+                    // Obtém o nome do arquivo
+                    $filename = basename($_FILES['imageFile']['name']);
+                    
+                    // Insere o nome do arquivo na coluna 'profile_picture' da tabela 'users'
+                    $sql = "UPDATE users SET profile_picture = '$filename' WHERE id=1";
 
-                // Read the binary data from the file
-                $binaryData = file_get_contents($fileTmp);
-
-                // Prepare the query
-                $query = "UPDATE users SET profile_picture = (?) WHERE id = 1";
-                $stmt = $conn->prepare($query);
-                $stmt->bind_param('b', $binaryData);
-
-                // Execute the query
-                if ($stmt->execute()) {
-                    // Return a success response to the client
-                    $response = array('status' => 'success', 'message' => 'Image uploaded successfully');
+                    if ($conn->query($sql) === TRUE) {
+                        echo 'Imagem enviada e inserida no banco de dados com sucesso.';
+                    } else {
+                        echo 'Erro ao inserir a imagem no banco de dados: ' . $conn->error;
+                    }  
                 } else {
-                    // Handle database error
-                    $response = array('status' => 'error', 'message' => 'Error uploading image');
+                    echo 'Erro ao mover o arquivo para o diretório de armazenamento.';
                 }
-
-                $stmt->close();
             } else {
-                // Handle case when no file was uploaded
-                $response = array('status' => 'error', 'message' => 'No image file provided');
+                echo 'Apenas arquivos de imagem são permitidos.';
             }
-
-            $conn->close();
+        } else {
+            echo 'Erro no envio do arquivo.';
         }
+        $conn->close();
     }
 }
 
